cc8172140409ce9f18b13773537c810daa2848e2
Michael Simacek <msimacek@redhat.com>
2016-09-05 11:23:47 +0200

    First attempt at Continuous Delivery (#1)

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -28,7 +28,18 @@ cd gh-pages
 rm -f latest
 ln -s "$latest" latest
 git add -A
-git commit -m 'Rebuild documentation'
-git push ../.. gh-pages:gh-pages
-cd ../..
-echo "Upload the documentation with 'git push origin gh-pages:gh-pages'"
+if [ -n "$TRAVIS_BRANCH" ]; then
+    if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+        # deploy from travis
+        git commit -m 'Rebuild documentation' --author 'Travis CI <fedora-java@users.noreply.github.com>'
+        openssl aes-256-cbc -K $encrypted_1f9369ab557d_key -iv $encrypted_1f9369ab557d_iv -in travis-key.enc -out travis-key -d
+        chmod 600 travis-key
+        ssh-agent sh -c 'ssh-add travis-key && git push git@github.com:fedora-java/howto.git gh-pages:gh-pages'
+    fi
+else
+    # probaly executed by human
+    git commit -m 'Rebuild documentation'
+    git push ../.. gh-pages:gh-pages
+    cd ../..
+    echo "Upload the documentation with 'git push origin gh-pages:gh-pages'"
+fi
