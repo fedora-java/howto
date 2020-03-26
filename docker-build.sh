c5ab76c68c6c87f51f407b9d0b73cc41535970bd
terminus-brut <marian.koncek@mailbox.org>
2020-03-26 11:04:02 +0100

    Apply requested changes

--- docker-build.sh
+++ docker-build.sh
@@ -2,29 +2,29 @@
 
 set -e
 
-echo "${COMMIT_MESSAGE}"
+# variable ${COMMIT_MESSAGE} provided from the outside
 
 # --setopt=tsflags=
 # Reason: Reset dnf flags in order ot install documentation files including manpages
 dnf -y --setopt=tsflags= install asciidoc dia git javapackages-local javapackages-tools m4 make man python3-ansi2html
 
-git config --global user.email mkoncek@redhat.com
-git config --global user.name mkoncek-jenkins
+git config --global user.name 'mkoncek-jenkins'
+git config --global user.email '<>'
 
 export GIT_SSH_COMMAND='/usr/bin/ssh -i /mnt/build/jenkins.private -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 
-git clone 'https://github.com/fedora-java/howto.git' &
+git clone 'https://github.com/fedora-java/howto.git'
 git clone 'ssh://git@pagure.io/java-packaging-howto.git'
 
 pushd howto
 make antora
 popd
 
-rm -rf java-packaging-howto/modules
+git rm -rf java-packaging-howto/modules
 mv howto/modules java-packaging-howto
 
 pushd java-packaging-howto
 git add modules
-git commit -m "${COMMIT_MESSAGE}" &&
+git commit -m "${COMMIT_MESSAGE}"
 git push origin
 popd
